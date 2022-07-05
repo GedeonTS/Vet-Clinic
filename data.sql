db3da8dc70299730dd2ff8d78cdf5bc4c2dadb25
GedeonTS <gedeontshobohwa1@gmail.com>
2022-07-05 23:14:18 +0200

    populate data into the table

--- data.sql
+++ data.sql
@@ -1,5 +1,7 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+
+INSERT INTO animals VALUES(1,'Agumon','03/02/2020', 0, true,10.23);
+INSERT INTO animals VALUES(1,'Gabumon','15/11/2018', 2, true,8);
+INSERT INTO animals VALUES(1,'Pikachu','07/01/2021', 1, false,15.04);
+INSERT INTO animals VALUES(1,'Devimon','12/05/2017', 5, true,11);
