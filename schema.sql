aa00b7e7efe634b84d0d0d7af2b27cb0fca7927f
GedeonTS <gedeontshobohwa1@gmail.com>
2022-07-05 23:01:27 +0200

    create table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,12 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+    id int,
+    name varchar(100),
+    date_of_birth date, 
+    escape_attemps int, 
+    neutered boolean, 
+    weight_kg dec
 );
+
+
