dcfda200109e3e5836502e53ac4327984ca22c25
GedeonTS <gedeontshobohwa1@gmail.com>
2022-07-06 09:27:47 +0200

    add data to the table

--- data.sql
+++ data.sql
@@ -2,6 +2,6 @@
 
 
 INSERT INTO animals VALUES(1,'Agumon','03/02/2020', 0, true,10.23);
-INSERT INTO animals VALUES(1,'Gabumon','15/11/2018', 2, true,8);
-INSERT INTO animals VALUES(1,'Pikachu','07/01/2021', 1, false,15.04);
-INSERT INTO animals VALUES(1,'Devimon','12/05/2017', 5, true,11);
+INSERT INTO animals VALUES(1,'Gabumon','11/15/2018', 2, true,8);
+INSERT INTO animals VALUES(1,'Pikachu','01/07/2021', 1, false,15.04);
+INSERT INTO animals VALUES(1,'Devimon','05/12/2017', 5, true,11);
