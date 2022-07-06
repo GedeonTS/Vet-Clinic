c1717a48cead4276629e4ab9d6625e1ba76b0100
GedeonTS <gedeontshobohwa1@gmail.com>
2022-07-06 16:33:27 +0200

    add data to the table

--- data.sql
+++ data.sql
@@ -2,6 +2,15 @@
 
 
 INSERT INTO animals VALUES(1,'Agumon','03/02/2020', 0, true,10.23);
-INSERT INTO animals VALUES(1,'Gabumon','11/15/2018', 2, true,8);
-INSERT INTO animals VALUES(1,'Pikachu','01/07/2021', 1, false,15.04);
-INSERT INTO animals VALUES(1,'Devimon','05/12/2017', 5, true,11);
+INSERT INTO animals VALUES(2,'Gabumon','11/15/2018', 2, true,8);
+INSERT INTO animals VALUES(3,'Pikachu','01/07/2021', 1, false,15.04);
+INSERT INTO animals VALUES(4,'Devimon','05/12/2017', 5, true,11);
+
+
+INSERT INTO animals VALUES(5,' Charmander','02/08/2020', 0, false,11);
+INSERT INTO animals VALUES(6,'Plantmon','11/15/1993', 2, true,5.7);
+INSERT INTO animals VALUES(7,'Squirtle','04/02/1993', 3, false,12.13);
+INSERT INTO animals VALUES(8,'Angemon','06/12/2005', 1, true,45);
+INSERT INTO animals VALUES(9,'Boarmon','06/07/2005', 7, true, 20.4);
+INSERT INTO animals VALUES(10,'Blossom','10/13/1998', 3, true,17);
+INSERT INTO animals VALUES(11,'Ditto','05/14/2022', 4, true,22);
