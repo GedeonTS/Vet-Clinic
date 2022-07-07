284947bfe60c8c6821b8c4985ad097f3ad401d24
GedeonTS <gedeontshobohwa1@gmail.com>
2022-07-07 19:06:23 +0200

    insert data to the tables

--- data.sql
+++ data.sql
@@ -14,3 +14,15 @@ INSERT INTO animals VALUES(8,'Angemon','06/12/2005', 1, true,45);
 INSERT INTO animals VALUES(9,'Boarmon','06/07/2005', 7, true, 20.4);
 INSERT INTO animals VALUES(10,'Blossom','10/13/1998', 3, true,17);
 INSERT INTO animals VALUES(11,'Ditto','05/14/2022', 4, true,22);
+
+INSERT INTO species(name) VALUES('Pokemon'),('Digimon');
+INSERT INTO owners(full_name,age) VALUES('Sam Smith',34),('Jennifer Orwell',19),('Bob',45),('Melody Pond',77),('Dean Winchester', 14),('Jodie Whittaker',38);
+
+UPDATE animals SET owner_id=1 WHERE name='Agumon';
+UPDATE animals SET owner_id=2 WHERE name='Gabumon' OR name='Pikachu';
+UPDATE animals SET owner_id=3 WHERE name='Devimon' OR name='Plantmon';
+UPDATE animals SET owner_id=4 WHERE name='Charmander' OR name='Squirtle' OR name='Blossom';
+UPDATE animals SET owner_id=5 WHERE name='Angemon' OR name='Boarmon';
+
+
+
